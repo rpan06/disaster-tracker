09c0347e4675a473bffda9bd4eb17041057e0a75
citation0097 <32308475+citation0097@users.noreply.github.com>
2018-09-05 11:42:26 -0700

    news api v0.2

--- new.js
+++ new.js
@@ -9,7 +9,7 @@ const NEWS_URL = 'https://newsapi.org/v2/everything';
 const API_KEY  =  '5f9b798c24b344edb04feb52d0e31d4f';
 
 var data_object = { apiKey: API_KEY,
-    q: "",    from: "",    to: "", sortBy: ""
+    q: "",    from: "",    to: "", sortBy: "" , pageSize: ""
 };
 
 var ajaxParams = {
@@ -22,29 +22,23 @@ var ajaxParams = {
 }
 
 function getResult(response){
-
-        if(response.success){
-            for( let i  in  response.data) {
-                var    newsData = new Object();
-                newsData.status = response.data[i].status;
-                newsData.totalResults = response.data[i].totalResults;
-                newsData.articles =  response.data[i].articles;
-                newsData.source =  response.data[i].source;
-                newsData.author =  response.data[i].author;
-                newsData.title =  response.data[i].title;
-                newsData.description =  response.data[i].description;
-                newsData.url =  response.data[i].url;
-                newsData.urlToImage =  response.data[i].urlToImage;
-                newsData.publishedAt =  response.data[i].publishedAt;
-                news_array.push(newsData);
-                console.log ('news_array', news_array);
-                // updateStudentList(student_array);
-
-            }
-            console.log('msg success', response);
-        }else{
-            console.log('msg fail', response);
+    var    newsData = new Object();
+    if(response.status === 'ok'){
+        for( let i  in  response.articles) {
+            newsData.source =  response.articles[i].source;
+            newsData.author =  response.articles[i].author;
+            newsData.title =  response.articles[i].title;
+            newsData.description =  response.articles[i].description;
+            newsData.url =  response.articles[i].url;
+            newsData.urlToImage =  response.articles[i].urlToImage;
+            newsData.publishedAt =  response.articles[i].publishedAt;
+            news_array.push(newsData);
         }
+        console.log ('news_array', news_array);
+        console.log('msg success', response);
+    }else{
+        console.log('msg fail', response);
+    }
 }
 
 function requsetData(  ) {
@@ -53,6 +47,7 @@ function requsetData(  ) {
     data_object.from = "2018-07-01";
     data_object.to = "2018-09-04";
     data_object.sortBy = "relevancy";
+    data_object.pageSize = '15';
     ajaxParams.url = NEWS_URL;
     ajaxParams.data = data_object;
 }
