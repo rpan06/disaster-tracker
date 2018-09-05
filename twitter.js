f4f11be4a1338bcd95dd0a68fac7e38852054d52
Rachel Pan <rpan06@gmail.com>
2018-09-05 16:52:13 -0700

    fixed Twitter hyperlinks

--- twitter.js
+++ twitter.js
@@ -13,27 +13,32 @@ function searchTwitter(keyWord = "world disaster"){
     };
     $.ajax(ajaxConfig);
 }
-//need input topic/location from map;
-//searchTwitter(inputFromMapMarker);
+
 searchTwitter();
 
 function renderTweets(dataReceived) {
     $('#tab2default').empty()
-    console.log(dataReceived)
-
-    if(!dataReceived.tweets.statuses.length){
+    let tweetData = dataReceived.tweets.statuses
+    if(!tweetData.length){
         $('<div>').text("No Tweets Found On This Topic").appendTo("#tab2default")
     }
-    for (let i = 0; i < dataReceived.tweets.statuses.length; i++){
-        let tweetText = dataReceived.tweets.statuses[i].text;
-        if(tweetText.indexOf("https://t.co/") !== -1){
-            tweetText = tweetText.split(" ")
-            tweetText[tweetText.length-1] = tweetText[tweetText.length-1].link(tweetText[tweetText.length-1])
-            tweetText = tweetText.join(" ")
+
+    for (let i = 0; i < tweetData.length; i++){
+        let tweetText = tweetData[i].text;
+
+        //if a retweet, then use original text
+        if (tweetData[i].retweeted_status){
+            tweetText = tweetData[i].retweeted_status.text
+        }
+
+        //makes hyperlinks
+        if(tweetText.indexOf("https://") !== -1){
+            let linkText = tweetText.match(/https\S+/)[0]
+            tweetText = tweetText.replace(linkText, linkText.link(linkText))
         }
 
         let twitterName = dataReceived.tweets.statuses[i].user.screen_name;
-        let tweetNameDiv = $('<div>').text(twitterName).addClass('tweetName');
+        let tweetNameDiv = $('<div>').text(`@${twitterName}`).addClass('tweetName');
         let tweetTextDiv = $('<div>').html(tweetText).addClass('tweets');
         $('#tab2default').append(tweetNameDiv, tweetTextDiv);
     }
