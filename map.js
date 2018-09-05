7c26a3486d8022961f3bd0c155ae3267a2231461
Rachel Pan <rpan06@gmail.com>
2018-09-05 13:30:58 -0700

    added popupwindow

--- map.js
+++ map.js
@@ -16,11 +16,25 @@ function initMap() {
 
 function placeMarkers(array) {
   for (let i = 0; i < array.length; i++) {
-    var coordinates = array[i].location;
-    var latLng = new google.maps.LatLng(coordinates.lat,coordinates.lon);
-    var marker = new google.maps.Marker({
+    const item = array[i];
+    let latLng = new google.maps.LatLng(item.location.lat,item.location.lon);
+    let infowindow = new google.maps.InfoWindow({
+      content: '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      `<h2 id="firstHeading" class="firstHeading">${item.title}</h2>`+
+      '<div id="bodyContent">'+
+      `<p>${item.body} <a href=${item.url}>read more</a></p>`+
+      '</div>'+
+      '</div>'
+    });
+    let marker = new google.maps.Marker({
       position: latLng,
-      map: map
+      map: map,
+      title: item.title
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
     });
   }
 }
